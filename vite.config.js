dd6a9187d42c018d54bdce8348a3ccd9b792ae0f
Eneias da Fonseca Dias <edias.dias@terra.com.br>
2025-03-27 23:57:13 -0300

    fix: porta correta para o Render detectar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0', // 👈 Isso é essencial pro Render detectar!
+  }
+})
