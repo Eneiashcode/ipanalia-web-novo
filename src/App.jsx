9f094e382525ff60bd8c75a080874a223bc78651
Eneias da Fonseca Dias <edias.dias@terra.com.br>
2025-03-27 23:41:07 -0300

    fix: remove node_modules e adiciona .gitignore correto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+// src/pages/Home.jsx
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-900">Igreja Presbiteriana Anália</h1>
+    </div>
+  );
+}
